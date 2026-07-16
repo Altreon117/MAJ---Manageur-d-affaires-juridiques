dc99378627045bac209ca404d75026374f7adf93
Altreon117 <rahp13450@gmail.com>
2026-07-16 15:53:32 +0200

    initialisation du projet

---
+++ config.py
@@ -0,0 +1,6 @@
+APP_NAME = "MAJ - Manageur d'affaires juridiques"
+CHECK_INTERVAL_MS = 600000  # 10 minutes en millisecondes
+
+# URLs et Chemins
+URL_SUIVI_POSTE = "https://www.laposte.fr/outils/suivre-vos-envois"
+EXCEL_FILE_PATH = ".xlsx"
